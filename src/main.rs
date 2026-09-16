1db3ea5518bd837c1184f2be2649b40835d6d479
svnzzz <svnzation@gmail.com>
2026-09-15 18:26:44 -0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,14 @@
-use smithay::reexports::wayland_server::Display;
-use smithay::reexports::wayland_server::ListeningSocket;
+use smithay::reexports::wayland_server::{Display, ListeningSocket};
 use calloop::EventLoop;
 
 
 // Smithay
+// Our listening socket manages clients connecting to the compositor
 
 fn main(){
 	// wayland display
 	let display = Display::<()>::new().unwrap();
+	let display_handle = display.handle();
 
 	// iterate to bind our socket
 	let socket = ListeningSocket::bind_auto("wayland", 0..10).unwrap();
@@ -17,7 +18,9 @@ fn main(){
 	
 	println!("Wayland display created: {:p}, socket as well: {:p}", &display, &socket);
 	println!("\n Socket name: {:?}", socket.socket_name());
+	println!("\n Display Handle: {:?}", display_handle);
 
+	// wait for stuff to happen...
 	event_loop
 		.run(None, &mut (), |_| {})
 		.unwrap();
