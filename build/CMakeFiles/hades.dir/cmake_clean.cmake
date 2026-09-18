a5e3d8c478cdd32a54844f1551687582a3477d2b
svnzzz <svnzation@gmail.com>
2026-09-17 20:42:24 -0700

    update

---
+++ build/CMakeFiles/hades.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hades.dir/link.d"
+  "CMakeFiles/hades.dir/src/main.cpp.o"
+  "CMakeFiles/hades.dir/src/main.cpp.o.d"
+  "hades"
+  "hades.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hades.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/hades.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for hades.
+# This may be replaced when dependencies are built.
